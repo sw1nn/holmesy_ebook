31c8dd982e97890ac9c652aad431490f603c99ce
Neale Swinnerton <neale@isismanor.com>
2013-07-30 21:18:25 +0100

    Initial Checkin

---
+++ src/holmesy_ebooks/core.clj
@@ -0,0 +1,44 @@
+(ns holmesy-ebooks.core
+    (:use
+   [twitter.oauth]
+   [twitter.callbacks]
+   [twitter.callbacks.handlers]
+   [twitter.api.restful])
+  (:import
+   (twitter.callbacks.protocols SyncSingleCallback)))
+
+;; (def my-creds (make-oauth-creds
+;;                *app-consumer-key*
+;;                *app-consumer-secret*
+;;                *user-access-token*
+;;                *user-access-token-secret*))
+
+(def my-creds nil)
+
+(defn tweet [t]
+  (statuses-update :oauth-creds my-creds
+                   :params {:status t}))
+
+(defn split-into-sentences [book]
+  (drop 1000 (re-seq #"[A-Z][A-Za-z, !;:'\?]{10,140}" book)))
+
+(defn random-sentence [book]
+  (let [sentences (split-into-sentences book)
+        num (count sentences)]
+    (fn [] (nth sentences (rand-int num) ))))
+
+(defn main-twitter [& [tweet]]
+  (let [fn (random-sentence (slurp "resources/sherlock-holmes.txt"))]
+    (while true
+      (let [text (fn)]
+        (when tweet) (tweet text)
+        (println text))
+      (Thread/sleep 1000))))
+
+(comment
+
+(main-twitter)
+
+(main-twitter :tweet)
+
+)
---
+++ test/horsey_ebooks/core_test.clj
@@ -0,0 +1,7 @@
+(ns horsey-ebooks.core-test
+  (:require [clojure.test :refer :all]
+            [horsey-ebooks.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
